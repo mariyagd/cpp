ebae59a820d10d3fd811fbacf4574b86079c34d8
mariyagd <109855801+mariyagd@users.noreply.github.com>
2023-12-21 04:17:47 +0100

    ex01 program arguments correction

--- c00/ex01/main.cpp
+++ c00/ex01/main.cpp
@@ -1,7 +1,13 @@
 #include "phone.h"
 
-int main(void)
+int main(int ac, char **av)
 {
+	(void)av;
+	if (ac > 1)
+	{
+		std::cerr << BOLD << "Please open this program without any arguments." << RESET << std::endl;
+		return 1;
+	}
 	PhoneBook	PhoneBook;
 	return 0;
 }
